4e425c75e3930177c2e7ff099d4fdb02fb269151
Mikhail Fedosov <tbs.micle@gmail.com>
2022-04-26 15:29:24 +0300

    chore: code formatting

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    quoteProps: "consistent",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "always",
+};
