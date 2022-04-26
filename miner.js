bcbc6ad312aad4b0d7f505851940f3f41f6132e3
Mikhail Fedosov <tbs.micle@gmail.com>
2022-04-26 15:36:36 +0300

    feat: random rocks

--- miner.js
+++ miner.js
@@ -1,7 +1,10 @@
 import RockObj from "./miner/rock_obj.js";
 import Rock from "./miner/rock.js";
+import randomArray from "random-array";
 
 const rock_obj = new RockObj();
+const seed = Math.round(randomArray(0, 1000000).oned(1)[0]);
+rock_obj.seed = seed;
 console.log(rock_obj);
 const rock = new Rock(rock_obj);
 console.log(rock);
