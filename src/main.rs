a2a7b805539b6db95a1a9efb8c09ffad7b8939b3
3dpass <3dpass@example.com>
2022-08-16 18:09:49 +0300

    Link with p3d library v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::fs::File;
+use std::io::Read;
 use structopt::StructOpt;
-// use std::fs::File;
-// use std::io::Write;
 
 use p3d::p3d_process;
 use p3d::AlgoType;
@@ -24,10 +24,6 @@ struct Cli {
     #[structopt(short, long, parse(from_os_str))] //, default_value = "data/st1.obj")]
     /// The path to the file to read
     infile: std::path::PathBuf,
-
-    // #[structopt(short, long, parse(from_os_str))] //, default_value = "data/st1.hash")]
-    // // The path to the file to save results
-    // outfile: std::path::PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -35,19 +31,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut res_hashes = Ok(vec![]);
     if args.algo == "grid2d" {
-       res_hashes = p3d_process(&args.infile, AlgoType::Grid2d, args.grid, args.sect); //, None);
+        let mut file = File::open(&args.infile).expect("No file found");
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).expect("Can't read inout file");
+
+        res_hashes = p3d_process(buffer.as_slice(), AlgoType::Grid2d, args.grid, args.sect);
     }
 
     let hashes = res_hashes.unwrap();
 
-    // match std::fs::write(&args.outfile, hashes) {
-    //     Ok(()) => { println!("write file OK"); },
-    //     Err(error) => { return Err(error.into());  }
-    // }
-    // let mut f = File::create(&args.outfile)?;
-
     for h in &hashes {
-        // write!(f, "{}", h)?;
         println!("{:?}", h);
     }
     Ok(())
